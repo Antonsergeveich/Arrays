d472417bf643b07cf28f605f9b9cc41cc2d3b4d5
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-02-05 21:36:15 +0300

    introduction to arryes

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -4,5 +4,21 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
+	cout << "Hello Arrays" << endl;
 
+    const int SIZE = 5;
+	cout << "¬ведите значение массива: ";
+	int n = 0;
+	{
+		for (int i = 0; i < SIZE; i++)
+			cin >> n;
+	}
+	int arr[SIZE] = {n};
+	cout << int() << endl;
+	
+	for (int i = 0; i < SIZE; i++)
+	{
+	cout << arr[i] << "\t";
+	}
+	cout << endl;
 }
