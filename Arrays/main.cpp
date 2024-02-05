de9959e8b999cad60b5389030f0369a912329ad8
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-02-05 22:22:15 +0300

    1234

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,24 +1,20 @@
 #include<iostream>
 using namespace std;
-
+using std::cin;
 void main()
 {
 	setlocale(LC_ALL, "");
 	cout << "Hello Arrays" << endl;
-
-    const int SIZE = 5;
-	cout << "¬ведите значение массива: ";
-	int n = 0;
-	{
-		for (int i = 0; i < SIZE; i++)
-			cin >> n;
-	}
-	int arr[SIZE] = {n};
-	cout << int() << endl;
-	
+	const int SIZE = 5;
+    int arr[SIZE] = {};
+	cout << " ¬ведите значени€ элементов массива: ";
 	for (int i = 0; i < SIZE; i++)
 	{
-	cout << arr[i] << "\t";
+		cin >> arr[i];
+	}
+	for (int i = 0; i < SIZE; i++)
+	{
+	    cout << arr[i] << "\t";
 	}
 	cout << endl;
 }
