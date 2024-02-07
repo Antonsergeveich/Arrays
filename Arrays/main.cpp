89688cfcc9f97b890debd6709fe96111f1907fea
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-02-07 22:23:28 +0300

    introduction to arryes

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,20 +1,77 @@
 #include<iostream>
 using namespace std;
-using std::cin;
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	cout << "Hello Arrays" << endl;
 	const int SIZE = 5;
     int arr[SIZE] = {};
-	cout << " ¬ведите значени€ элементов массива: ";
+	//вывод значений элементов массива с клавиатуры
+	cout << "¬ведите значени€ элементов массива: ";
 	for (int i = 0; i < SIZE; i++)
 	{
-		cin >> arr[i]; 
+		cin >> arr[i];
 	}
+	cout << endl;
+	//вывод элементов массива в пр€мом пор€дке
+	cout << "Ёлементы массива в пр€мом пор€дке: ";
 	for (int i = 0; i < SIZE; i++)
 	{
 	    cout << arr[i] << "\t";
 	}
+	cout << endl;
+	cout << endl;
+	//вывод элементов массива в обратном пор€дке
+	cout << "Ёлементы массива в обратном пор€дке: ";
+	for (int i = SIZE-1; i != -1; i--)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+	cout << endl;
+	//сумма всех элементов массива
+	int sum = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+	    sum = sum + arr[i];
+	}
+	cout << "—умма всех элементов массива: " << sum << endl;
+	cout << endl;
+	//вычисл€ем среднее-арифметическое элементов массива
+	double s = 0;
+	double sa;
+	for (int i = 0; i < SIZE; i++)
+	{
+		s = s + arr[i];
+	}
+	sa = s / SIZE;
+	cout << "—реднее арифметическое элементов массива: " << sa << endl;
+	cout << endl;
+
+	//находим минимальное и максимальное значение элементов и выводим их на экран
+	int x = arr[0];
+	for (int i = 0; i < SIZE; i++)
+	{
+		x = arr[i];
+	}
+	for (int i = 1; i < SIZE; i++)
+	{
+		if (x > arr[i])
+		{
+			x = arr[i];
+		}
+	}
+	cout << "Ёлемент массива с наименьшим значением: " << x << "\t";
+	cout << endl;
+	cout << endl;
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (x < arr[i])
+		{
+			x = arr[i];
+		}
+	}
+	cout << "Ёлемент массива с наибольшим значением: " << x << "\t";
 	cout << endl;
 }
